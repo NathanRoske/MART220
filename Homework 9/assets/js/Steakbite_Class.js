8951055d445487ee2171e291fae03be3961f72b4
NathanRoske <nathanroske@outlook.com>
2025-03-31 21:41:24 -0600

    WIP Homework 8
    Not fully functional, an attempt was made.

---
+++ Homework 9/assets/js/Steakbite_Class.js
@@ -0,0 +1,15 @@
+class Steakbites
+{
+    constructor(x,y)
+    {
+        this.x = x;
+        this.y = y;
+        this.SteakbitePiece = new Sprite(x, y, 25)
+        
+    }
+
+    draw()
+    {
+        this.SteakbitePiece.color = "maroon";
+    }
+}
---
+++ Homework 9/assets/js/class animationImages.js
@@ -0,0 +1,76 @@
+class animationImages
+{
+    constructor(x, y, w, h)
+    {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.currentAnimation;
+        this.createAnimation();
+        this.direction = "";
+        this.speed = 3;
+        
+        
+        
+    }
+
+    
+    createAnimation()
+    {
+        this.currentAnimation = createSprite(this.x, this.y);
+    }
+
+    getCurrentAnimation()
+    {
+        return this.currentAnimation;
+    }
+
+    loadAnimation(animationType, fileNames)
+    {
+        this.currentAnimation.addAnimation(animationType, fileNames[0], fileNames[fileNames.length - 1]); 
+        this.currentAnimation.width = 150;
+        this.currentAnimation.height = 150;  
+    }
+
+    drawAnimations(animationType)
+    {
+        this.currentAnimation.frameDelay = 5000;
+        this.currentAnimation.scale = .25; 
+        this.currentAnimation.changeAnimation(animationType);
+        if (animationType == 'walk' && this.direction == 'forward') {
+            this.currentAnimation.direction = 0;
+            this.currentAnimation.mirror.x = false;
+            this.currentAnimation.speed = this.speed;
+        }
+        else if (animationType == 'walk' && this.direction == 'reverse') {
+            this.currentAnimation.mirror.x = true;
+            this.currentAnimation.direction = 180;
+            this.currentAnimation.speed = this.speed;
+        }
+        else if (animationType == 'walk' && this.direction == 'down') {
+            this.currentAnimation.mirror.x = true;
+            this.currentAnimation.direction = 90;
+            this.currentAnimation.speed = this.speed;
+        }
+        else if (animationType == 'walk' && this.direction == 'up') {
+            this.currentAnimation.direction = 270;
+            this.currentAnimation.mirror.x = false;
+            this.currentAnimation.speed = this.speed;
+        }
+        else {
+            this.currentAnimation.velocity.x = 0;
+            this.currentAnimation.velocity.y = 0;
+        }
+    }
+
+    updatePosition(direction)
+    {
+        this.direction = direction;   
+    }
+    
+    isColliding(myBarrier) {
+        return this.currentAnimation.collide(myBarrier);
+    }
+        
+}
